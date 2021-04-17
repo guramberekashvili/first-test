59a8a259ad0e07bc5366744df14f418b94aa5f17
guramberekashvili <guraberekashvili@gmail.com>
2021-04-17 14:27:01 +0400

    firstbranch one

---
+++ test 2/src/Homework/Main.java
@@ -0,0 +1,24 @@
+package Homework;
+
+import java.util.Locale;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String txt_1 = "Hello World";
+        int i = 0;
+        for ( i =0; i<11;i++);{
+
+
+
+
+            System.out.println(i);
+            i++;
+
+        }
+    }
+
+
+    }
+
